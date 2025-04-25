bfba63b65c1f1686df53d5d48c3b58d9fae9f223
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-04-25 14:37:40 -0500

    Implemented Template Pattern.

--- Gang of Four Patterns/TemplateMethod/Implementation.cs
+++ Gang of Four Patterns/TemplateMethod/Implementation.cs
@@ -1,3 +1,62 @@
 ﻿namespace TemplateMethod
-{ 
+{
+    /// <summary>
+    /// Abstract Class
+    /// </summary>
+    public abstract class MailParser
+    {
+        public virtual void FindServer()
+        {
+            Console.WriteLine("Finding server...");
+        }
+
+        public abstract void AuthenticateToServer();
+        public string ParseHtmlMailBody(string identifier)
+        {
+            Console.WriteLine("Parsing HTML mail body...");
+            return $"This is the body of the email with the id: {identifier}";
+        }
+
+        /// <summary>
+        /// Template method
+        /// </summary>
+        /// <param name="identifier"></param>
+        /// <returns></returns>
+        public string ParseMailBody(string identifier)
+        {
+            Console.WriteLine("Parsing mail body (in template method)...");
+            FindServer();
+            AuthenticateToServer();
+            return ParseHtmlMailBody(identifier);
+        }
+    }
+
+    public class ExchangeMailParser : MailParser
+    {
+        public override void AuthenticateToServer()
+        {
+            Console.WriteLine("Connecting to Exchange server...");
+        }
+    }
+
+    public class ApacheMailParser : MailParser
+    {
+        public override void AuthenticateToServer()
+        {
+            Console.WriteLine("Connecting to Apache server...");
+        }
+    }
+
+    public class EudoraMailParser : MailParser
+    {
+        public override void FindServer()
+        {
+            Console.WriteLine("Finding Eudora server via custom algorithm...");
+        }
+
+        public override void AuthenticateToServer()
+        {
+            Console.WriteLine("Connecting to Eudora server...");
+        }
+    }
 }
--- Gang of Four Patterns/TemplateMethod/Program.cs
+++ Gang of Four Patterns/TemplateMethod/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.Title = "Template Method";
+﻿using TemplateMethod;
+
+Console.Title = "Template Method";
+
+ExchangeMailParser exchangeMailParser = new();
+Console.WriteLine(exchangeMailParser.ParseMailBody("1234567890"));
+Console.WriteLine("--------------------------------------------------");
+
+ApacheMailParser apacheMailParser = new();
+Console.WriteLine(apacheMailParser.ParseMailBody("0987654321"));
+Console.WriteLine("--------------------------------------------------");
+
+EudoraMailParser eudoraMailParser = new();
+Console.WriteLine(eudoraMailParser.ParseMailBody("1122334455"));
+Console.WriteLine("--------------------------------------------------");
